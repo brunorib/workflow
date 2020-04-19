2bd5c85b8762c22e6a077ad705eb584bbd3f32e5
Bruno Ribeiro <brunoribca@gmail.com>
2020-04-19 14:26:39 +0200

    migrations upgrade

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p /app/migrations/versions
+
 # Apply database upgrades
 echo "Apply database migrations"
-python manage.py db upgrade
+python manage.py db migrate
 
 # Start server
 echo "Starting server"
