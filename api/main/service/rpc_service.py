d90b764ed18bb359c6549411b07ec490f01c8895
Bruno Ribeiro <brunoribca@gmail.com>
2020-04-22 11:44:00 +0200

    prints

--- api/main/service/rpc_service.py
+++ api/main/service/rpc_service.py
@@ -14,10 +14,13 @@ class RpcClient(object):
         result = self.channel.queue_declare(queue='callback', exclusive=True)
         self.callback_queue = result.method.queue
 
+        print("instantiation")
+
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
             auto_ack=True)
+        print("instantiation2")
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
@@ -25,6 +28,7 @@ class RpcClient(object):
 
     def call(self, json_object):
         self.response = None
+        print("call")
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
             exchange='',
@@ -34,6 +38,7 @@ class RpcClient(object):
                 correlation_id=self.corr_id,
             ),
             body=json.dumps(json_object))
+        print("call2")
         while self.response is None:
             self.connection.process_data_events()
         return self.response
