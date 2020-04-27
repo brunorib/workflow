eec3970fd30d42045925357cd83585d23503a414
Bruno Ribeiro <brunoribca@gmail.com>
2020-04-27 17:40:24 +0200

    threading and closing

--- api/main/service/rpc_service.py
+++ api/main/service/rpc_service.py
@@ -35,13 +35,13 @@ class RpcClient(object):
          to RPC requests.
         """
         self.stop_event = threading.Event()
-        self.listening_thread  = threading.Thread(target=self._process_data_events, args=(self.stop_event,))
+        self.listening_thread  = threading.Thread(target=self._process_data_events)
         self.listening_thread.setDaemon(True)
         self.listening_thread.start()
 
     def _process_data_events(self, stop_event):
         """Process Data Events using the Process Thread."""
-        while not stop_event.is_set():
+        while not self.stop_event.is_set():
             self.channel.start_consuming()
 
     def _on_response(self, message):
